b2b5e4e034118afed9c4563c60c7fbc9c46053a4
wm <wm@163.com>
2020-09-01 18:43:05 +0800

    EDIT BY B

--- atguigu-redis/src/main/java/com/atguigu/redis/RedisTest1.java
+++ atguigu-redis/src/main/java/com/atguigu/redis/RedisTest1.java
@@ -7,7 +7,7 @@ public class RedisTest1 {
         System.out.println("hello=====master");
         System.out.println("hello=====hotfix");
         System.out.println("hello");//test push
-        System.out.println("hello");
+        System.out.println("hello");//EDIT BY B
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
