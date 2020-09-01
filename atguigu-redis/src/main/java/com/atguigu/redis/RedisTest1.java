b4b30a5d88f02f94992b873ae20bd0c62baec328
wm <wm@163.com>
2020-09-01 17:09:55 +0800

    新建类

---
+++ atguigu-redis/src/main/java/com/atguigu/redis/RedisTest1.java
@@ -0,0 +1,14 @@
+package com.atguigu.redis;
+
+public class RedisTest1 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
