cca0ebcec1e04a351278d03b992c34d420c9aeca
wm <wm@163.com>
2020-09-01 17:12:12 +0800

    修改类--hotfix

--- atguigu-redis/src/main/java/com/atguigu/redis/RedisTest1.java
+++ atguigu-redis/src/main/java/com/atguigu/redis/RedisTest1.java
@@ -4,7 +4,7 @@ public class RedisTest1 {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello");
-        System.out.println("hello");
+        System.out.println("hello=====hotfix");
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
